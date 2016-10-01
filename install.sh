8f34bd4763a0e89738649f75faa0d661d1933895
Valounours <valounours@gmail.com>
2016-10-01 20:56:19 +0200

    Mise a jour - Ajout de l'option Sous-Domaine

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 DIRDATA='/home'
+NDD='localhost'
 clear
 # Check Root
 if [ $(id -u) != "0" ]; then
@@ -24,7 +25,8 @@ maison_pgrm () {
 	"SickRage" "Logiciel d'automatisation de téléchargement de séries"  \
 	"CouchPotato" "Logiciel d'automatisation de téléchargement de Films" \
 	"Plex" "Logiciel de streaming multimédia" \
-	"ESM" "Monitorer votre serveur" 5>&1 1>&2 2>&5)
+	"ESM" "Monitorer votre serveur" \
+	"Sous-domaines" "Acceder au service sans les ports" 5>&1 1>&2 2>&5)
  
 	exitstatus=$?
 	if [ $exitstatus = 0 ]; then
@@ -46,6 +48,9 @@ maison_pgrm () {
 		fi
 		if [ $OPTION = ESM ]; then
 			esm
+		fi
+		if [ $OPTION = Sous-domaines ]; then
+			subdomain
 		fi
 	else
 		clear
@@ -73,16 +78,17 @@ sed -i "s|^.*\"incomplete-dir-enabled\":.*|    \"incomplete-dir-enabled\": true\
 if (whiptail --yesno "Acces par l'interface WEB ?" 8 78 --title "Media Server Recovery") then
 	sed -i 's/.*"rpc-whitelist.enabled":.*/    "rpc-whitelist-enabled": 'false',/' /etc/transmission-daemon/settings.json
 	sed -i 's/.*"rpc-enabled":.*/    "rpc-enabled": 'true',/' /etc/transmission-daemon/settings.json
+	TRANSWEBUSER=$(whiptail --inputbox "Nom d'utilisateur ? (Pour l'interface WEB)" 8 78 "transmission" --title "Media Server Recovery" 3>&1 1>&2 2>&3)
+	sed -i 's/.*"rpc-username":.*/    "rpc-username": '\"$TRANSWEBUSER\"',/' /etc/transmission-daemon/settings.json
+	TRANSWEBPASS=$(whiptail --inputbox "Mot de passe ? (Pour l'interface WEB)" 8 78 "motdepasse" --title "Media Server Recovery" 3>&1 1>&2 2>&3)
+	sed -i 's/.*"rpc-password":.*/    "rpc-password": '\"$TRANSWEBPASS\"',/' /etc/transmission-daemon/settings.json
+	service transmission-daemon start
+	whiptail --title "Media Server Recovery" --msgbox "Installation et réglages de TRANSMISSION finis.\n \nTransmission est accessible depuis : http://IP:9091" 10 60
+	maison_pgrm
 else
+	service transmission-daemon start
 	exit 1
 fi
-TRANSWEBUSER=$(whiptail --inputbox "Nom d'utilisateur ? (Pour l'interface WEB)" 8 78 "transmission" --title "Media Server Recovery" 3>&1 1>&2 2>&3)
-sed -i 's/.*"rpc-username":.*/    "rpc-username": '\"$TRANSWEBUSER\"',/' /etc/transmission-daemon/settings.json
-TRANSWEBPASS=$(whiptail --inputbox "Mot de passe ? (Pour l'interface WEB)" 8 78 "motdepasse" --title "Media Server Recovery" 3>&1 1>&2 2>&3)
-sed -i 's/.*"rpc-password":.*/    "rpc-password": '\"$TRANSWEBPASS\"',/' /etc/transmission-daemon/settings.json
-service transmission-daemon start
-whiptail --title "Media Server Recovery" --msgbox "Installation et réglages de TRANSMISSION finis.\n \nTransmission est accessible depuis : http://IP:9091" 10 60
-maison_pgrm
 }
 
 sickrage () {
@@ -130,6 +136,64 @@ whiptail --title "Media Server Recovery" --msgbox "Installation et réglages de 
 maison_pgrm
 }
 
+subdomain () {
+a2enmod proxy
+a2enmod proxy_http
+/etc/init.d/apache2 restart
+cd /etc/apache2/sites-available
+mv 000-default.conf 000-default.conf.base
+cat >> 000-default.conf <<EOF
+<VirtualHost *:80>
+	# The ServerName directive sets the request scheme, hostname and port that
+	# the server uses to identify itself. This is used when creating
+	# redirection URLs. In the context of virtual hosts, the ServerName
+	# specifies what hostname must appear in the request's Host: header to
+	# match this virtual host. For the default virtual host (this file) this
+	# value is not decisive as it is used as a last resort host regardless.
+	# However, you must set it for any further virtual host explicitly.
+	#ServerName www.example.com
+
+	ServerAdmin webmaster@localhost
+	DocumentRoot /var/www/html
+
+	# Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+	# error, crit, alert, emerg.
+	# It is also possible to configure the loglevel for particular
+	# modules, e.g.
+	#LogLevel info ssl:warn
+
+	ErrorLog ${APACHE_LOG_DIR}/error.log
+	CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+	# For most configuration files from conf-available/, which are
+	# enabled or disabled at a global level, it is possible to
+	# include a line for only one particular virtual host. For example the
+	# following line enables the CGI configuration for this host only
+	# after it has been globally disabled with "a2disconf".
+	#Include conf-available/serve-cgi-bin.conf
+</VirtualHost>
+
+# vim: syntax=apache ts=4 sw=4 sts=4 sr noet
+
+ProxyPass /couchpotato http://localhost:5050/couchpotato
+ProxyPassReverse /couchpotato http://localhost:5050/couchpotato
+
+ProxyPass /transmission http://localhost:9091/transmission
+ProxyPassReverse /transmission http://localhost:9091/transmission
+
+ProxyPass /sickrage http://localhost:8081/home
+ProxyPassReverse /sickrage http://localhost:8081/home
+EOF
+/etc/init.d/apache2 restart
+service couchpotato stop
+sed -i '21s/url_base=/url_base=\/couchpotato/' /var/opt/couchpotato/setting.conf
+service couchpotato start
+service sickrage stop
+sed -i '802s/web_root = ""/web_root = "\/couchpotato/' /opt/sickrage/config.init
+service sickrage start
+whiptail --title "Media Server Recovery" --msgbox "Les sous-domaines sont actif :\n \nTransmission : *NDD/transmission \nSickrage : $NDD/sickrage \nCouchPotato : $NDD/couchpotato \n Plex : $NDD/plex" 10 60
+maison_pgrm
+}
 whiptail --title "Bienvenue" --msgbox "Créé par Valounours. Contact : Valounours@gmail.com" 10 60
 if (whiptail --title "Media Server Recovery" --yesno "Voulez-vous continuer ?" 10 60) then
 	OPTION=$(whiptail --title "Media Server Recovery" --menu "Faites votre choix ?" 15 80 6 \
@@ -138,7 +202,8 @@ if (whiptail --title "Media Server Recovery" --yesno "Voulez-vous continuer ?" 1
 	"SickRage" "Logiciel d'automatisation de téléchargement de séries"  \
 	"CouchPotato" "Logiciel d'automatisation de téléchargement de Films" \
 	"Plex" "Logiciel de streaming multimédia" \
-	"ESM" "Monitorer votre serveur" 5>&1 1>&2 2>&5)
+	"ESM" "Monitorer votre serveur" \
+	"Sous-domaines" "Acceder au service sans les ports" 5>&1 1>&2 2>&5)
  
 	exitstatus=$?
 	if [ $exitstatus = 0 ]; then
@@ -160,6 +225,9 @@ if (whiptail --title "Media Server Recovery" --yesno "Voulez-vous continuer ?" 1
 		fi
 		if [ $OPTION = ESM ]; then
 			esm
+		fi
+		if [ $OPTION = Sous-domaines ]; then
+			subdomain
 		fi
 	else
 		clear
