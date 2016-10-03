630adf9d3553dcc3a5f9d4a872eef86bc3c9a13f
root <root@debian>
2016-10-03 18:30:02 +0200

    Message de validation

--- install.sh
+++ install.sh
@@ -60,7 +60,7 @@ lamp () {
 debconf-apt-progress -- apt-get install apache2 libapache2-mod-php5 mysql-server php5-mysql phpmyadmin -y
 a2enmod proxy
 a2enmod proxy_http
-etc/init.d/apache2 restart
+/etc/init.d/apache2 restart
 whiptail --title "Media Server Recovery" --msgbox "Votre serveur WEB est pret." 10 60
 maison_pgrm
 }
@@ -71,8 +71,9 @@ mkdir -p  $DIRDATA/torrent/{"encours","fini","watch"}
 chgrp debian-transmission $DIRDATA/torrent/{"encours","fini","watch"}
 chmod -R 770 $DIRDATA/torrent/{"encours","fini","watch"}
 echo -e '\n#proxy transmission \nProxyPass /transmission http://localhost:9091/transmission \nProxyPassReverse /transmission http://localhost:9091/transmission' >> /etc/apache2/sites-available/000-default.conf
-etc/init.d/apache2 restart
+/etc/init.d/apache2 restart
 service transmission-daemon stop
+sleep 1
 #Réglages de l'acces WEB
 if (whiptail --yesno "Acces par l'interface WEB ?" 8 78 --title "Media Server Recovery") then
 	sed -i 's/.*"rpc-whitelist.enabled":.*/    "rpc-whitelist-enabled": 'false',/' /etc/transmission-daemon/settings.json
@@ -89,6 +90,10 @@ if (whiptail --yesno "Acces par l'interface WEB ?" 8 78 --title "Media Server Re
 	whiptail --title "Media Server Recovery" --msgbox "Installation et réglages de TRANSMISSION finis.\n \nTransmission est accessible depuis : http://IP/transmission" 10 60
 	maison_pgrm
 else
+	#Réglages des dossiers
+	sed -i "s|^.*\"download-dir\":.*|    \"download-dir\": \"$DIRDATA\/torrent\/fini\"\,|" /etc/transmission-daemon/settings.json
+	sed -i "s|^.*\"incomplete-dir\":.*|    \"incomplete-dir\": \"$DIRDATA\/torrent\/encours\"\,|" /etc/transmission-daemon/settings.json
+	sed -i "s|^.*\"incomplete-dir-enabled\":.*|    \"incomplete-dir-enabled\": true\,|" /etc/transmission-daemon/settings.json
 	service transmission-daemon start
 	exit 1
 fi
@@ -103,7 +108,13 @@ chmod +x /etc/init.d/sickrage
 echo -e "SR_USER=root\nSR_HOME=/opt/sickrage\nSR_DATA=/opt/sickrage\nSR_GROUP=root" >>  /etc/default/sickrage
 update-rc.d sickrage defaults
 service sickrage start
-whiptail --title "Media Server Recovery" --msgbox "Installation et réglages de SICKRAGE finis.\n \nSickrage est accessible depuis : http://IP:8081" 10 60
+echo -e '\n#proxy sickrage \nProxyPass /sickrage http://localhost:8081/sickrage \nProxyPassReverse /sickrage http://localhost:8081/sickrage' >> /etc/apache2/sites-available/000-default.conf
+/etc/init.d/apache2 restart
+service sickrage stop
+sed -i -e 's/web_root = ""/web_root = \/sickrage/g' /opt/sickrage/config.ini
+sed -i -e 's/handle_reverse_proxy = 0/handle_reverse_proxy = 1/g' /opt/sickrage/config.ini
+service sickrage restart
+whiptail --title "Media Server Recovery" --msgbox "Installation et réglages de SICKRAGE finis.\n \nSickrage est accessible depuis : http://IP/sickrage" 10 60
 maison_pgrm
 }
 
@@ -116,7 +127,12 @@ echo -e "CP_HOME=/opt/couchpotato\nCP_USER=root" >> /etc/default/couchpotato
 chmod +x /etc/init.d/couchpotato
 update-rc.d couchpotato defaults
 service couchpotato start
-whiptail --title "Media Server Recovery" --msgbox "Installation et réglages de COUCHPOTATO finis.\n \nCouchPotato est accessible depuis : http://IP:5050" 10 60
+sleep 1
+sed -i -e "s/url_base =/url_base = \/couchpotato/g" /var/opt/couchpotato/settings.conf
+service couchpotato restart
+echo -e '\n#proxy couchpotato \nProxyPass /couchpotato http://localhost:5050/couchpotato \nProxyPassReverse /couchpotato http://localhost:5050/couchpotato' >> /etc/apache2/sites-available/000-default.conf
+/etc/init.d/apache2 restart
+whiptail --title "Media Server Recovery" --msgbox "Installation et réglages de COUCHPOTATO finis.\n \nCouchPotato est accessible depuis : http://IP:couchpotato" 10 60
 maison_pgrm
 }
 
@@ -131,72 +147,16 @@ maison_pgrm
 }
 
 esm () {
+cd /tmp
 wget --content-disposition  http://ezservermonitor.com/esm-web/downloads/version/2.5
 unzip ezservermonitor-web_v2.5.zip
-mv eZServerMonitor-2.5/ esm
-cp esm /var/www/html
+mv eZServerMonitor-2.5/ esm/
+cp -R esm/ /var/www/html
+chown -R www-data.www-data /var/www/html/esm
 whiptail --title "Media Server Recovery" --msgbox "Installation et réglages de ESM finis.\n \nESM est accessible depuis : http://NDD/esm" 10 60
 maison_pgrm
 }
 
-subdomain () {
-a2enmod proxy
-a2enmod proxy_http
-/etc/init.d/apache2 restart
-cd /etc/apache2/sites-available
-mv 000-default.conf 000-default.conf.base
-cat >> 000-default.conf <<EOF
-<VirtualHost *:80>
-	# The ServerName directive sets the request scheme, hostname and port that
-	# the server uses to identify itself. This is used when creating
-	# redirection URLs. In the context of virtual hosts, the ServerName
-	# specifies what hostname must appear in the request's Host: header to
-	# match this virtual host. For the default virtual host (this file) this
-	# value is not decisive as it is used as a last resort host regardless.
-	# However, you must set it for any further virtual host explicitly.
-	#ServerName www.example.com
-
-	ServerAdmin webmaster@localhost
-	DocumentRoot /var/www/html
-
-	# Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
-	# error, crit, alert, emerg.
-	# It is also possible to configure the loglevel for particular
-	# modules, e.g.
-	#LogLevel info ssl:warn
-
-	ErrorLog ${APACHE_LOG_DIR}/error.log
-	CustomLog ${APACHE_LOG_DIR}/access.log combined
-
-	# For most configuration files from conf-available/, which are
-	# enabled or disabled at a global level, it is possible to
-	# include a line for only one particular virtual host. For example the
-	# following line enables the CGI configuration for this host only
-	# after it has been globally disabled with "a2disconf".
-	#Include conf-available/serve-cgi-bin.conf
-</VirtualHost>
-
-# vim: syntax=apache ts=4 sw=4 sts=4 sr noet
-
-ProxyPass /couchpotato http://localhost:5050/couchpotato
-ProxyPassReverse /couchpotato http://localhost:5050/couchpotato
-
-ProxyPass /transmission http://localhost:9091/transmission
-ProxyPassReverse /transmission http://localhost:9091/transmission
-
-ProxyPass /sickrage http://localhost:8081/home
-ProxyPassReverse /sickrage http://localhost:8081/home
-EOF
-/etc/init.d/apache2 restart
-service couchpotato stop
-sed -i '21s/url_base=/url_base=\/couchpotato/' /var/opt/couchpotato/setting.conf
-service couchpotato start
-service sickrage stop
-sed -i '802s/web_root = ""/web_root = "\/couchpotato/' /opt/sickrage/config.init
-service sickrage start
-whiptail --title "Media Server Recovery" --msgbox "Les sous-domaines sont actif :\n \nTransmission : *NDD/transmission \nSickrage : $NDD/sickrage \nCouchPotato : $NDD/couchpotato \n Plex : $NDD/plex" 10 60
-maison_pgrm
-}
 whiptail --title "Bienvenue" --msgbox "Créé par Valounours. Contact : Valounours@gmail.com" 10 60
 if (whiptail --title "Media Server Recovery" --yesno "Voulez-vous continuer ?" 10 60) then
 	OPTION=$(whiptail --title "Media Server Recovery" --menu "Faites votre choix ?" 15 80 6 \
